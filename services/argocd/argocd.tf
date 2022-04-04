32b3804d6a005b307b20ca2d6bebb47d2b00ad27
Stephen Wolfe <stephenwolfe1@gmail.com>
2022-04-04 01:06:47 -0700

    Set branch to develop

--- services/argocd/argocd.tf
+++ services/argocd/argocd.tf
@@ -6,7 +6,7 @@ module "argocd" {
   argocd_hostname           = "argocd.swolfe.io"
   argocd_admin_groups       = ["admins"]
   cluster_bootstrap_repo    = "https://github.com/stephenwolfe1/argocd-applications.git"
-  target_revision           = "setup"
+  target_revision           = "develop"
   github_username           = local.github_username
   github_token              = local.github_token
   github_oauth_clientID     = local.github_oauth_clientID
